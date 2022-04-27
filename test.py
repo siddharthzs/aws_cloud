feb1cb426fc536f4ceea5742d7c2c8209c5910d9
siddharthzs <siddharthchoudhary.mail@gmail.com>
2022-04-28 02:32:24 +0530

    learned more on test

--- test.py
+++ test.py
@@ -6,3 +6,11 @@
 
 from bisect import bisect_left
 
+def take_closest(myList, myNumber):
+    """
+    Assumes myList is sorted. Returns closest value to myNumber.
+
+    If two numbers are equally close, return the smallest number.
+    """
+    pos = bisect_left(myList, myNumber)
+    if pos == 0:
