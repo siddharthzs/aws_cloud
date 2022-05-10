bc9303e82731879c162e6816161462a8c41cf526
siddharthzs <siddharthchoudhary.mail@gmail.com>
2022-05-11 02:40:56 +0530

    learning index

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function flip(arr) {
   let res = [];
   console.log(count1s);
   while (ipos <= jpos && jpos < arr.length) {
+    if (arr[jpos] == "0") curCount++;
+    else curCount--;
+
+    if (curCount < count1s) {ipos = jpos + 1; curCount = count1s}
+    else if (curCount > maxCount) {
+      maxCount = curCount;
+      res = [ipos + 1, jpos + 1];
+    }
+
+    jpos++;
