ef0866b60bd0002fb65d323049b6b72df8f1e42f
siddharthzs <siddharthchoudhary.mail@gmail.com>
2022-06-12 02:45:36 +0530

    learned more on gitcommit

--- gitcommit.py
+++ gitcommit.py
@@ -8,3 +8,13 @@ fileNames = [
     "gitcommit.py",
     "IAM.md"
 ]
+
+# s = subprocess.call('git init', cwd='D:/cloud-aws/aws_cloud')
+
+curFile = fileNames.pop(rd.randrange(len(fileNames)))
+OutFile = open(f'aws_cloud/{curFile}','a')
+InFile = open(curFile,'r')
+date = 13
+fileContent = InFile.readlines()
+i = 1
+x = rd.randrange(5)
