c8926c64d46250114748f7c1e0a5a56b3acc135f
siddharthzs <siddharthchoudhary.mail@gmail.com>
2022-07-16 00:38:30 +0530

    continue on gitcommit

--- gitcommit.py
+++ gitcommit.py
@@ -75,3 +75,13 @@ for _ in range(31):
                     s = subprocess.call(f'git add . && git commit --date "{date} day ago" -m "learning {curFile[:-3]}"', cwd='D:/cloud-aws/aws_cloud',  shell=True)
                 elif(y > 1  and y <= 3):
                     s = subprocess.call(f'git add . && git commit --date "{date} day ago" -m "learned more on {curFile[:-3]}"', cwd='D:/cloud-aws/aws_cloud',  shell=True)
+                elif(y > 3):
+                    s = subprocess.call(f'git add . && git commit --date "{date} day ago" -m "continue on {curFile[:-3]}"', cwd='D:/cloud-aws/aws_cloud',  shell=True)
+                OutFile = open(f'aws_cloud/{curFile}','a')
+                x-=1
+                if(x == 0):
+                    date = date-1
+                    x = 3
+            i+=1
+
+        OutFile.close()
